0acec87e6811cc8c58e2dd6bd273967f336ce713
idiot-stick <luqinzhe@gmail.com>
2025-10-05 18:15:51 -0500

    Update sorting.algorithm.cc

--- sorting.algorithm.cc
+++ sorting.algorithm.cc
@@ -1,16 +1,18 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 int main()
 {
-	std::vector<int> pre_sort_nums {3, 2, 5, 1, 4};  //Vector that holds pre-sorted numbers
-	std::vector<int> post_sort_nums {};              //Vector that holds sorted numbers
+	std::vector<int> pre_sort_nums {3, 2, 5, 1, 4};
+	std::vector<int> post_sort_nums {};            
 	
 	int pre_sort_nums_max_size = pre_sort_nums.size();
+	int smallest_num {};
 
 	for (int j = 0; j < pre_sort_nums_max_size; ++j)
 	{
-		int smallest_num = pre_sort_nums[0];
+		smallest_num = pre_sort_nums[0];
 		for (int i = 0; i < pre_sort_nums.size(); ++i)
 		{
 			if (pre_sort_nums[i] < smallest_num)
@@ -20,23 +22,15 @@ int main()
 			
 		}
 		
-		for (int i = 0; i < pre_sort_nums.size(); ++i)
-		{
-			if (pre_sort_nums[i] == smallest_num)
-			{
-				pre_sort_nums.erase(pre_sort_nums.begin() + i);
-				
-			}
-		}
+		pre_sort_nums.erase(std::remove(pre_sort_nums.begin(), pre_sort_nums.end(), smallest_num), pre_sort_nums.end());
+		
 		post_sort_nums.emplace_back(smallest_num);
-  	}
+  }
 
   for (int i = 0; i < post_sort_nums.size(); ++i)
   {
-    	std::cout<<post_sort_nums[i]<<"\n";
+    std::cout<<post_sort_nums[i]<<"\n";
   }
 
 	return 0;
 }
-
-
