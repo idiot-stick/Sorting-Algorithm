d434b5aafcbb541d126b52b40ab4918360a24e83
Matthew Lu <luqinzhe@gmail.com>
2025-10-04 21:06:32 -0500

    yay its fixed

--- sorting.algorithm.cc
+++ sorting.algorithm.cc
@@ -3,37 +3,37 @@
 
 int main()
 {
-	std::vector<int> foo {3, 2, 5, 1, 4}; //Vector that holds pre-sorted numbers
-	std::vector<int> goo {};              //Vector that holds sorted numbers
+	std::vector<int> pre_sorted_nums {3, 2, 5, 1, 4}; //Vector that holds pre-sorted numbers
+	std::vector<int> post_sorted_nums {};              //Vector that holds sorted numbers
 	
-	int smallest_num = {foo[0]};
-	int foo_max_size = foo.size();
+	int pre_sorted_nums_max_size = pre_sorted_nums.size();
 
-	for (double j; j < foo_max_size; ++j) //It correctly prints without this for loop, but doesn't print anything with the for loop
+	for (int j = 0; j < pre_sorted_nums_max_size; ++j) //It correctly prints without this for loop, but doesn't print anything with the for loop
 	{
-		for (double i; i < foo.size(); ++i)
+    int smallest_num = pre_sorted_nums[0];
+		for (int i = 0; i < pre_sorted_nums.size(); ++i)
 		{
-			if (foo[i] < smallest_num)
+			if (pre_sorted_nums[i] < smallest_num)
 			{
-				smallest_num = foo[i];
+				smallest_num = pre_sorted_nums[i];
 			}
 			
 		}
 		
-		for (double i; i < foo.size(); ++i)
+		for (int i = 0; i < pre_sorted_nums.size(); ++i)
 		{
-			if (foo[i] == smallest_num)
+			if (pre_sorted_nums[i] == smallest_num)
 			{
-				foo.erase(foo.begin() + i);
+				pre_sorted_nums.erase(pre_sorted_nums.begin() + i);
 				
 			}
 		}
-		goo.emplace_back(smallest_num);
+		post_sorted_nums.emplace_back(smallest_num);
 	}
 
-  for (double i; i < goo.size(); ++i)
+  for (int i = 0; i < post_sorted_nums.size(); ++i)
   {
-    std::cout<<goo[i]<<"\n";
+    std::cout<<post_sorted_nums[i]<<"\n";
   }
 
 	return 0;
