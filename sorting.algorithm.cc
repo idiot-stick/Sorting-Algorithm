a475d5f6a31eb4b140d9f466152faa70acb30e46
idiot-stick <luqinzhe@gmail.com>
2025-10-04 20:50:47 -0500

    Update sorting.algorithm.cc

--- sorting.algorithm.cc
+++ sorting.algorithm.cc
@@ -6,11 +6,11 @@ int main()
 	std::vector<int> pre_sort_nums {3, 2, 5, 1, 4};  //Vector that holds pre-sorted numbers
 	std::vector<int> post_sort_nums {};              //Vector that holds sorted numbers
 	
-	int smallest_num = {pre_sort_nums[0]};
 	int pre_sort_nums_max_size = pre_sort_nums.size();
 
 	for (int j = 0; j < pre_sort_nums_max_size; ++j)
 	{
+		int smallest_num = pre_sort_nums[0];
 		for (int i = 0; i < pre_sort_nums.size(); ++i)
 		{
 			if (pre_sort_nums[i] < smallest_num)
